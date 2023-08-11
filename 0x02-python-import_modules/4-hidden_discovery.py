eb85a8d23336f2ba213a3b968b8ec83554409fff
mustafa slama <ayman1slama@gmail.com>
2023-08-11 11:10:31 +0300

    fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import hidden_4
 
 if __name__ == "__main__":
-    import hidden_4
+
     list = dir(hidden_4)
+
     for func in list:
         if func[:2] != "__":
             print("{:s}".format(func))
