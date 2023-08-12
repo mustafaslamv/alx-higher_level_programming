aebef1ae5798d43eed9b94eee4ba364cd8366758
mustafa slama <ayman1slama@gmail.com>
2023-08-12 17:33:12 +0300

    add condition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-	if len(matrix) == 0:
-		print("")
-	else:
-		for ROWitems in matrix:
-			for Citems in ROWitems:
-				print("{:d}".format(Citems), end=" ")
-			print(end="\n")
+    if len(matrix) == 0:
+        print("")
+    else:
+        for ROWitems in matrix:
+            for Citems in ROWitems:
+                print("{:d}".format(Citems), end=" ")
+            print("")
