3a7638b19bbffcb0bf769f3b818f6fee3fdda592
mustafa slama <ayman1slama@gmail.com>
2023-08-17 00:34:19 +0300

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-  """Returns a new matrix where each element is the square of the corresponding element in the original matrix."""
-
-  new_matrix = []
-  for row in matrix:
-    new_row = []
-    for element in row:
-      new_row.append(element ** 2)
-    new_matrix.append(new_row)
-
-  return new_matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
