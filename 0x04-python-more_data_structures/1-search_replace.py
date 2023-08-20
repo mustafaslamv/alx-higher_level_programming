6ec2bcbd05af5328f6d19c53761e81fa7d3aaedc
mustafa slama <ayman1slama@gmail.com>
2023-08-20 10:42:07 +0300

    ternary operator

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,7 @@
 def search_replace(my_list, search, replace):
 
     def find_search(element):
-        if element == search:
-            return replace
-        else:
-            return element
+        return replace if element == search else element
 
     new_list = [find_search(element) for element in my_list]
 
