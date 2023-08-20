3c09aa3bac842077f26d4a2d31a438c518ff1b4e
mustafa slama <ayman1slama@gmail.com>
2023-08-20 19:44:08 +0300

    use map() instead

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
-    def find_search(element):
-        return replace if element == search else element
-
-    new_list = [find_search(element) for element in my_list]
-
-    return new_list
+    my_list = list(map((lambda x: replace if x == search else x), my_list))
+    return my_list
