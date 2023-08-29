6950a9441923f0497f4ac90c05a8aba0d3529ef6
mustafa slama <ayman1slama@gmail.com>
2023-08-29 18:27:24 +0300

    task0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(0, x):
+            print(my_list[i], end='')
+            count += 1
+        print("")
+    except IndexError:
+        print("")
+        pass
+    return count
