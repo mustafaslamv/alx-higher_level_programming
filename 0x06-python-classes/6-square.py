202ff969cdf0a82459605adc9ad14932bff99bbe
mustafa slama <ayman1slama@gmail.com>
2023-09-01 16:18:19 +0300

    add square position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""task: 5. Printing a square"""
+
+
+class Square:
+    """Square class"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+            position (tuple): tuple of 2 positive integers
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints in stdout the square with the character #.
+        """
+        if self.size == 0:
+            print("")
+        else:
+            for _ in range(self.position[1]):
+                print("")
+            for _ in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
