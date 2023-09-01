807def60dd25eb9545540e341902c3afdbedf37c
mustafa slama <ayman1slama@gmail.com>
2023-09-01 11:39:12 +0300

    executable

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-	"""
-	Represents a square shape.
-	"""
-	def __init__(self):
-		"""
-		Initializes a new instance of the Square class.
-		"""
-		pass
+    "Empty class"
+    pass
