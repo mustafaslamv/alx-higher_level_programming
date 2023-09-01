44a27a460cdedce2b62d1e28e390c2d486e6d92e
mustafa slama <ayman1slama@gmail.com>
2023-09-01 11:47:05 +0300

    task2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""task: 1. Square with size"""
+
+
+class Square:
+    """class Square that defines a square"""
+
+    def __init__(self, size):
+        """
+Initializes a new instance of the Square class.
+
+Args:
+size (int): The size of the square.
+"""
+        self.__size = size
