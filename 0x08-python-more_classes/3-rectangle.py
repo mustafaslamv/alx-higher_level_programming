e1e5234336dc15ffb19a8a8cd1953f44ef44516a
mustafa slama <ayman1slama@gmail.com>
2023-09-04 23:22:21 +0300

    __str__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Task: 3. String representation"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Rectangle area instance method"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """Rectangle perimeter instance method"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """return class instance as a Rectangle W * H made of '#'"""
+        result = ""
+        for i in range(self.height):
+            result += ("#" * self.width)
+            if i != self.height-1:
+                result += "\n"
+        return result
