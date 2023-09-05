1d7529762f247bd51e49ea89fea5ebb94c473813
mustafa slama <ayman1slama@gmail.com>
2023-09-06 00:03:10 +0300

    quick edit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Task: 30. Low memory cost"""
+
+
 class LockedClass(object):
+    """class LockedClass with no class or object attribute"""
 
     __slots__ = ["first_name"]
 
-    def __init__(self, first_name=""):
-        self.first_name = first_name
+    def __init__(self):
+        pass
