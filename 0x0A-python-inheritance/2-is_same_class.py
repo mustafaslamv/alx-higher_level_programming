83cc59dc8996ea4020f53f59c5b3b1636c5372a8
mustafa slama <ayman1slama@gmail.com>
2023-09-11 19:13:08 +0300

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Task: 2. Exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """function checks of an object is instance or not"""
+    return isinstance(obj, a_class)
