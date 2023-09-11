f508ec58ac826463ac32f8cfbeb7cd3d5dd62f1c
mustafa slama <ayman1slama@gmail.com>
2023-09-11 19:28:06 +0300

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Task: 3. Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if an object is instance or not."""
+    return isinstance(obj, a_class)
