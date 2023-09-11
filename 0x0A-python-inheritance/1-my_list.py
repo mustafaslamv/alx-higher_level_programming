63eae9f7b3e41e128bf4794edfe22401cf7cdf4b
mustafa slama <ayman1slama@gmail.com>
2023-09-11 18:47:30 +0300

    tested more and more test cases

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Task: 1. My list"""
-
+import doctest
 
 class MyList(list):
     """class MyList that inherits from list"""
@@ -8,3 +8,6 @@ class MyList(list):
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
         print(sorted(self))
+
+if __name__ == "__main__":
+    doctest.testfile("tests/1-my_list.txt")
