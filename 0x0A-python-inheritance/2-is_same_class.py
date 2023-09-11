608d7f9fe65f1e84e51428db4487d9534448728f
mustafa slama <ayman1slama@gmail.com>
2023-09-11 19:16:11 +0300

    type(-) instead of isinstance(-,-)

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,5 +3,5 @@
 
 
 def is_same_class(obj, a_class):
-    """function checks of an object is instance or not"""
-    return isinstance(obj, a_class)
+    """function checks of an object is exactly an instance or not"""
+    return type(obj) == a_class
