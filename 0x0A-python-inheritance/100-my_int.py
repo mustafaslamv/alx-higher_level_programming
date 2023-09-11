f47e4832ed139a253caba9c9e39d61981acf2f73
mustafa slama <ayman1slama@gmail.com>
2023-09-11 21:16:29 +0300

    advanced1

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task: 12.My integer"""
+
+class MyInt(int):
+	"""class overrides some operators from int class"""
+	def __eq__(self, __value: object) -> bool:
+		return not super().__eq__(__value)
+	def __ne__(self, __value: object) -> bool:
+		return not super().__ne__(__value)
