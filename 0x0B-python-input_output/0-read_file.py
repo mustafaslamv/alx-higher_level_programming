d670fa4875394a1c6856a5d83147e9f5037cc62d
mustafa slama <ayman1slama@gmail.com>
2023-09-12 19:41:17 +0300

    remove permision

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """function reads text file as UTF8 and prints it"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         lines = f.readlines()
         for i in lines:
             print(i)
