d6fdf57f5c5ed8503385c0400a04d4b9375ac343
mustafa slama <ayman1slama@gmail.com>
2023-09-18 23:50:59 +0300

    private variables is in getter only

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Task: 3. Validate attributes"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -8,10 +8,10 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
