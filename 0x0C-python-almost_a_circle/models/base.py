35afa3da80dbed716d9704629b41a7f2d025acf2
mustafa slama <ayman1slama@gmail.com>
2023-09-18 22:47:19 +0300

    base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Task: 1. Base class"""
+
+
+class Base:
+    """Base class for all the derived classes"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
