5e4f223c67d8ac98c1726ad9c2c3b1e6947e4c06
mustafa slama <ayman1slama@gmail.com>
2023-09-19 00:08:47 +0300

    +

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Task: 3. Validate attributes"""
-from base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -62,4 +62,4 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
-        return self.width * self.height
+        return (self.width * self.height)
