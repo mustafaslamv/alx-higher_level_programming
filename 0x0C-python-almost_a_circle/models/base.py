654d8843fbf32ca8e6140de741b08846949b7eca
mustafa slama <ayman1slama@gmail.com>
2023-09-20 21:34:46 +0300

    create instance from dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,3 +42,12 @@ class Base:
         else:
             result = json.loads(json_string)
         return result
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            instance = cls(1, 1, 1, 1)
+        elif cls.__name__ == "Square":
+            instance = cls(1, 1, 1)
+        instance.update(**dictionary)
+        return instance
