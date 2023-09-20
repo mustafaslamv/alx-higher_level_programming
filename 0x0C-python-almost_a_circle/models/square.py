aea6fd00492b70fc76740f7ac7220fe0dffe3ee1
mustafa slama <ayman1slama@gmail.com>
2023-09-20 20:18:56 +0300

    override update, replace 'width,height' with size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,13 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        attributes = ["id", "size", "x", "y"]
+        if args:
+            for i, value in enumerate(args):
+                setattr(self, attributes[i], value)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
