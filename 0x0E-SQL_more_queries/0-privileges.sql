9a79279c17ef7d6bc105b51b853b31e71e3df064
mustafa slama <ayman1slama@gmail.com>
2023-10-19 19:31:51 +0300

    remove union

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,7 +1,6 @@
 -- 0. My privileges!
 -- script that lists all privileges of the MySQL users
 
-SHOW GRANTS FOR user_0d_1@localhost
-UNION
+SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
 
