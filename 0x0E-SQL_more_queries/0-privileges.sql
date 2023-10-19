fc4efea935f7b4746fb0e0c74dae83ea650cc1cb
mustafa slama <ayman1slama@gmail.com>
2023-10-19 19:31:19 +0300

    add @host

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,7 @@
 -- 0. My privileges!
 -- script that lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1
+
+SHOW GRANTS FOR user_0d_1@localhost
 UNION
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_2@localhost;
 
