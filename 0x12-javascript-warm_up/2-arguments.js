a70b645584053ef76b7b11ce71f8ab50528ef745
mustafa slama <ayman1slama@gmail.com>
2023-11-15 23:03:54 +0200

    counting arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const argc = process.argv;
+const len = argc.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
