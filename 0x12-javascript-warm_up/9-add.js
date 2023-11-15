2f85ab6286ab85ef2630adc830305f1689ac21c0
mustafa slama <ayman1slama@gmail.com>
2023-11-16 00:22:49 +0200

    add arguments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+function add (a, b) {
+  console.log(a + b);
+}
+
+const a = Number(process.argv[2]);
+const b = Number(process.argv[3]);
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  add(a, b);
+}
