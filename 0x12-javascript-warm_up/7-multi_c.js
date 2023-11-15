f060812f8b45af2fb58a5e47927af2ff6d375173
mustafa slama <ayman1slama@gmail.com>
2023-11-16 00:02:12 +0200

    loop according to argument passed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const countNumber = Number(process.argv[2]);
+if (isNaN(countNumber)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let index = 0; index < countNumber; index++) {
+    console.log('C is fun');
+  }
+}
