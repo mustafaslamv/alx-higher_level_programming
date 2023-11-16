5894f0e83720470254211a1cd7ea5efea0030383
mustafa slama <ayman1slama@gmail.com>
2023-11-16 11:46:39 +0200

    sort function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arr = process.argv;
-arr.splice(0, 2);
+const arr = process.argv.slice(2).map(Number);
+console.log(arr);
 const len = arr.length;
 if (len === 0 || len === 1) {
   console.log(0);
 } else {
-  arr.sort();
+  arr.sort((a, b) => a - b);
   console.log(arr[len - 1]);
 }
