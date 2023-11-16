1c303383de282bdc4d65881c9b6f3fb2f83bfaa7
mustafa slama <ayman1slama@gmail.com>
2023-11-16 11:17:43 +0200

    js factorial function

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+function factorial (number) {
+  number = parseInt(number);
+  if (number == NaN) {
+    return 1;
+  } else if (number == 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+const result = factorial(333);
+console.log(result);
