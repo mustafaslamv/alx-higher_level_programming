97c16763ae263c756d7062164735999061bc6260
mustafa slama <ayman1slama@gmail.com>
2023-11-16 11:26:58 +0200

    semistandard fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,7 +5,7 @@ function factorial (number) {
   number = parseInt(number);
   if (isNaN(number)) {
     return 1;
-  } else if (number == 1) {
+  } else if (number === 1) {
     return 1;
   } else {
     return number * factorial(number - 1);
