2133a1a103487c4165c2f6319e16bddec7defe87
mustafa slama <ayman1slama@gmail.com>
2023-11-16 11:37:47 +0200

    biggest argument value

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arr = process.argv;
+arr.splice(0, 2);
+const len = arr.length;
+if (len === 0 || len === 1) {
+  console.log(0);
+} else {
+  arr.sort();
+  console.log(arr[len - 1]);
+}
