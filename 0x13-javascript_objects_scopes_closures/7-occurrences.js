c78b24638815e15d464b2a686d1c684aa186f0c9
mustafa slama <ayman1slama@gmail.com>
2023-11-17 21:05:03 +0200

    count all occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const len = list.length;
+  let counter = 0;
+  for (let i = 0; i < len; i++) {
+    if (list[i] === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+};
