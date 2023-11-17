d2da944420426678c90b6bd20945aae5802c5297
mustafa slama <ayman1slama@gmail.com>
2023-11-18 00:24:49 +0200

    sorted dict

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const reversedDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (reversedDict[occurrences] === undefined) {
+    reversedDict[occurrences] = [userId];
+  } else {
+    reversedDict[occurrences].push(userId);
+  }
+}
+
+console.log(reversedDict);
