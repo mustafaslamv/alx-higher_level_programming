0c186b2e922c33e59aceeb30b04dd99b57838a77
mustafa slama <ayman1slama@gmail.com>
2023-11-17 20:57:04 +0200

    inherit from Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const square = require('./5-square.js');
+class Square extends square {
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += 'C';
+        }
+        console.log(line);
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+module.exports = Square;
