db08c6dd6978371321eb306ec0ceae3b51bb9897
mustafa slama <ayman1slama@gmail.com>
2023-11-20 21:12:19 +0200

    replace .format with placeholder %s

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""script displays all values in the states where name matches the argument
+	but It's safe from MySQL injections!
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    stateName = sys.argv[4]
+
+    myConn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+    myCursor = myConn.cursor()
+    query = "SELECT * FROM states\
+             WHERE BINARY name = %s\
+             ORDER BY states.id ASC"
+    myCursor.execute(query, (stateName,))
+    for i in myCursor.fetchall():
+        print(i)
+    myCursor.close()
+    myConn.close()
