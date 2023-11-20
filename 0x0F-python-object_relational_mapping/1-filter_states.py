cfe1cf14ff7ca85b9057dc3bf3aadf7b3fea338c
mustafa slama <ayman1slama@gmail.com>
2023-11-20 20:47:28 +0200

    add like filter, names starts with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""script that lists all states with a name starting with N """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    myConn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name,
+        charset="utf8"
+    )
+    myCursor = myConn.cursor()
+    myCursor.execute("SELECT * FROM states\
+                     WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    for i in myCursor.fetchall():
+        print(i)
+    myCursor.close()
+    myConn.close()
