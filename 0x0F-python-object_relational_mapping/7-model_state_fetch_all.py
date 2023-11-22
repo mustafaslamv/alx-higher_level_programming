c62296a086ef5094530a73ac37a51bcb03fb113e
mustafa slama <ayman1slama@gmail.com>
2023-11-22 23:53:52 +0200

    basic query using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name), pool_pre_ping=True)
+
+    Session = sessionmaker(engine)
+    session = Session()
+    result = session.query(State).all()
+    for ob in result:
+        print(f"{ob.id}: {ob.name}")
