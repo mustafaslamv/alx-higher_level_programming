1e45c151ab9032ce438e27c7b61a0c9c894f4d3a
mustafa slama <ayman1slama@gmail.com>
2023-11-23 00:40:40 +0200

    add new row

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""script that prints the State object with the name"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name), pool_pre_ping=True)
+    Session = sessionmaker(engine)
+    session = Session()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    result = session.query(State).order_by(State.id.desc()).first()
+    print(result.id)
