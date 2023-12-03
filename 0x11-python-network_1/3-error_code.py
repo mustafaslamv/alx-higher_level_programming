33d07595909cb57838dd0c30a967b08a517f3b3a
mustafa slama <ayman1slama@gmail.com>
2023-12-03 11:06:01 +0200

    handle HTTP errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Python script that sends HTTP request using urllib - 4"""
+from urllib import request
+from urllib import error
+from sys import argv
+
+if __name__ == "__main__":
+    hostname = argv[1]
+    try:
+        req = request.Request(hostname)
+        with request.urlopen(req) as response:
+            content = response.read().decode("UTF-8")
+            print(content)
+    except error.HTTPError as error:
+        print(f"Error code: {error.code}")
