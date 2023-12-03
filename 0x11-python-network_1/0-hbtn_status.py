1221839f1e9b7c4c9271dcf8ca7ed06f7490e4f3
mustafa slama <ayman1slama@gmail.com>
2023-12-03 09:42:48 +0200

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Python script that sends HTTP request using urllib"""
+from urllib import request
+
+if __name__ == "__main__":
+    req = request.Request("https://alx-intranet.hbtn.io/status")
+    with request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('UTF-8')}")
