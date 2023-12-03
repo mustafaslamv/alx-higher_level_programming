816f2cd56c5f0b0579cade8c4ce5bcb1f066809a
mustafa slama <ayman1slama@gmail.com>
2023-12-03 23:11:28 +0200

    i love requests, more and more

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""POST request using requests package"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    hostname = "http://0.0.0.0:5000/search_user"
+    q = argv[1] if len(argv) >= 2 else ""
+    request = requests.post(hostname, {'q': q})
+    dict = request.json()
+    if not dict:
+        print("No result")
+    else:
+        id = dict.get('id')
+        name = dict.get('name')
+        print(f"[{id}] {name}")
