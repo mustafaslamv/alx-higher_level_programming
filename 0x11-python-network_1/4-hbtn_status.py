fac6f5af6816acd591933c603317f0f7b4a01be4
mustafa slama <ayman1slama@gmail.com>
2023-12-03 21:49:37 +0200

    requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import requests
+
+request = requests.get("https://alx-intranet.hbtn.io/status")
+decoded_response = request.text
+print("Body response:")
+print(f"\t- type: {type(decoded_response)}")
+print(f"\t- content: {decoded_response}")
