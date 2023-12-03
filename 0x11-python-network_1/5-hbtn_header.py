bf9ffba28e0bc505c3b289da669c1f4e0cfc8a1d
mustafa slama <ayman1slama@gmail.com>
2023-12-03 21:53:40 +0200

    one specific header value print

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""GET request using requests package - 2"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    hostname = argv[1]
+    request = requests.get(hostname)
+    XRequestId = request.headers.get("X-Request-Id")
+    print(XRequestId)
